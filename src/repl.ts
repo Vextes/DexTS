0fa766c33c964e4d548866945b1126b21e664dd3
vextes <sjfrang@gmail.com>
2026-02-16 02:56:03 -0600

    Add basic REPL function

--- src/repl.ts
+++ src/repl.ts
@@ -1,4 +1,22 @@
+import { createInterface } from "node:readline";
+
 export function cleanInput(input: string): string[] {
     const words = input.trim().toLowerCase().split(' ').filter((word) => word !== '');
     return words;
+}
+
+export function startREPL() {
+    const rl = createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: "Pokedex > ",
+    });
+    rl.prompt();
+    rl.on("line", (line: string) => {
+        const cleanLine = cleanInput(line);
+        if (cleanLine.length > 0) {
+            console.log(`Your command was: ${cleanLine[0]}`);
+        }
+        rl.prompt();
+    })
 }
